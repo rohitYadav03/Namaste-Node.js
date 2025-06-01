6cb08f20ad372c4537875a1509d718d4b34a61ae
Rohit Yadav <rohityadav85801@gmail.com>
2025-06-01 09:47:43 +0000

    Episode 3

---
+++ app.js
@@ -0,0 +1,10 @@
+var name = "Rohit";
+
+var a = 10;
+var b = 20;
+// console.log(name,a+b);
+
+console.log(globalThis); 
+console.log(this); // empty object 
+
+// GlobalThis => this will be refer to global object 
